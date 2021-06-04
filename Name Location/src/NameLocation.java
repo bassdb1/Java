edf032db98d7952f05bb0fc23ba86f40ef6fc1b8
bassdb1 <d-bass@outlook.com>
2021-06-04 12:19:58 -0400

    Add files via upload

---
+++ Name Location/src/NameLocation.java
@@ -0,0 +1,11 @@
+
+public class NameLocation {
+
+	public static void main(String[] args) {
+		System.out.println("My name is David");
+		System.out.println("My hometown is Hensersonville, TN");
+
+
+	}
+
+}
