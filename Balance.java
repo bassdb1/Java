53fb314ab4e51a60d83b5888e7b3176c616eb8df
bassdb1 <d-bass@outlook.com>
2021-07-12 10:09:12 -0400

    Add files via upload

---
+++ Balance.java
@@ -0,0 +1,11 @@
+
+    public class Balance {
+        public static void main(String[] args){
+          Integer balance = 5;
+          balance = balance + 5;
+          balance = balance * 10;
+          System.out.println(balance);
+        }
+    }
+    
+
