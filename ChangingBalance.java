53fb83305a7ebfb6015549d597eb066ad6d2ee48
bassdb1 <d-bass@outlook.com>
2021-07-13 09:00:10 -0400

    Add files via upload

---
+++ ChangingBalance.java
@@ -0,0 +1,8 @@
+public class ChangingBalance {
+    public static void main(String[] args){
+        Integer balance = 5;        // after execution, value of balance: 5
+        balance = balance + 5;      // after execution, value of balance: 10
+        balance = balance * 10;     // after execution, value of balance: 100
+        System.out.println(balance);
+      }
+}
