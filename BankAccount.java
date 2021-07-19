9adcae95e6b731436ef8efa22f4626d08098f5d6
bassdb1 <d-bass@outlook.com>
2021-07-19 11:45:39 -0400

    Making new Bank Objects

---
+++ BankAccount.java
@@ -0,0 +1,10 @@
+class BankAccount {
+}
+public class Main {
+    public static void main(String[] args){
+      BankAccount firstAccount = new BankAccount();
+      BankAccount secondAccount = new BankAccount();
+      System.out.println(firstAccount);
+      System.out.println(secondAccount);
+    }
+}
